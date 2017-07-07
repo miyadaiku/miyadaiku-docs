30b129f2f1ba15afb3e0efcfb5929fcf9a7cdd21
Atsuo Ishimoto <atsuoishimoto@users.noreply.github.com>
2017-07-07 12:58:52 +0900

    Update deploy_ghp.sh

--- deploy_ghp.sh
+++ deploy_ghp.sh
@@ -3,12 +3,14 @@
 miyadaiku-build .
 
 REPO="git@github.com:miyadaiku/miyadaiku.github.io.git"
-
+git config --global user.email "autp@example.com"
+git config --global user.name "auto circleci"
+  
 rm -rf miyadaiku.github.io
 git clone ${REPO}
 cp -r outputs/* miyadaiku.github.io/
 cd miyadaiku.github.io
 git add --all *
-git commit -m "update doc" --author='autoupdate <circleci@example.com>'
+git commit -m "update doc"
 git push ${REPO} master
 rm -rf miyadaiku.github.io
