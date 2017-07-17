3287830e4a8bdab379b39a61c553a32c65203534
auto circleci <autp@example.com>
2017-07-17 20:54:03 +0900

    update build script

--- deploy_ghp.sh
+++ deploy_ghp.sh
@@ -5,20 +5,18 @@ git config --global user.email "autp@example.com"
 git config --global user.name "auto circleci"
   
 rm -rf miyadaiku.github.io || true
-git clone ${REPO}
 
-rm -rf miyadaiku.github.io/* || true
 miyadaiku-build doc
-cp -r doc/outputs/* miyadaiku.github.io/
-
-mkdir miyadaiku.github.io/samples
+mkdir doc/outputs/samples
 
 for d in samples/*/ ; do
   miyadaiku-build "$d"
-  mkdir miyadaiku.github.io/$d
-  cp -r $d/outputs/* miyadaiku.github.io/$d
+  mkdir doc/outputs/$d
+  cp -r $d/outputs/* doc/outputs/$d
 done
 
+git clone ${REPO}
+cp -r doc/outputs/* miyadaiku.github.io/
 cd miyadaiku.github.io
 git add --all *
 git commit -m "update doc" || true
