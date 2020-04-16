362d1de827283ad8f97b4d87f969251876d8c047
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-04-16 21:25:17 +0900

    heh

--- deploy_ghp.sh
+++ deploy_ghp.sh
@@ -1,18 +1,6 @@
 #!/bin/bash -e
 
 REPO="git@github.com:miyadaiku/miyadaiku.github.io.git"
-  
-rm -rf miyadaiku.github.io || true
-rm -rf doc/outputs/samples || true
-
-miyadaiku-build --rebuild doc
-
-mkdir doc/outputs/samples
-for d in samples/*/ ; do
-  miyadaiku-build --rebuild "$d"
-  mkdir doc/outputs/$d
-  cp -r $d/outputs/* doc/outputs/$d
-done
 
 git clone ${REPO}
 cp -r doc/outputs/* miyadaiku.github.io/
